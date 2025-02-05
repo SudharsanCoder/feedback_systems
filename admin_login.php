dc1b8923a905c75d1b572f0b8dfe53fd6a361076
SudharsanCoder <kongucoder24.7@gmail.com>
2025-02-05 20:36:33 +0530

    Create admin_login.php

---
+++ admin_login.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Admin Login</title>
+</head>
+<body>
+
+    <h2>Admin Login</h2>
+    <form action="admin_auth.php" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" required><br><br>
+        
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required><br><br>
+        
+        <button type="submit">Login</button>
+    </form>
+
+</body>
+</html>
